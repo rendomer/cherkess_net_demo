3282cd66e3db2ac7d7d0eff7f8f3a6eb96d62791
rendomer <abazg1@gmail.com>
2025-07-07 18:03:44 +0400

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,6 @@
 /// config.dart
+
+/// Адрес API сервера, можно переопределить через --dart-define=API_URL=your_url
 const String baseUrl = String.fromEnvironment(
   'API_URL',
   defaultValue: 'https://cherkessnetdemo-production.up.railway.app',
