b84608bc995952e70e4bcbba4d24ffbbe905a232
rendomer <abazg1@gmail.com>
2025-07-08 20:16:35 +0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,16 +3,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.app.routes.ping_routes import router as ping_router
-from backend.app.routes.user_routes import router as user_router
-from backend.app.routes.reset_password_routes import router as reset_password_router
-from backend.app.routes.email_routes import router as email_router
-from backend.app.routes.statistics_routes import router as statistics_router
-from backend.app.routes.survey_routes import router as survey_router
-from backend.app.database import init_models
-from backend.app.routes import debug_routes
-from backend.app.routes import stats_routes
-from backend.app.routes import people_routes
+from app.routes.ping_routes import router as ping_router
+from app.routes.user_routes import router as user_router
+from app.routes.reset_password_routes import router as reset_password_router
+from app.routes.email_routes import router as email_router  # ← Исправил!
+from app.routes.statistics_routes import router as statistics_router
+from app.routes.survey_routes import router as survey_router
+from app.database import init_models
+from app.routes import debug_routes
+from app.routes import stats_routes
+from app.routes import people_routes
 
 app = FastAPI()
 
@@ -23,6 +23,24 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.on_event("startup")
+async def on_startup():
+    await init_models()
+
+app.include_router(ping_router)
+app.include_router(user_router, prefix="/api/users", tags=["users"])
+app.include_router(reset_password_router, prefix="/api/reset-password", tags=["reset-password"])
+app.include_router(email_router)
+app.include_router(statistics_router, prefix="/api", tags=["statistics"])
+app.include_router(survey_router, prefix="/api/surveys", tags=["surveys"])
+app.include_router(debug_routes.router)
+app.include_router(stats_routes.router)
+app.include_router(people_routes.router, prefix="/api/people", tags=["People"])
+
+@app.get("/")
+async def root():
+    return {"message": "Сервер работает!"}
 
 
 @app.on_event("startup")
